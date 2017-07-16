51542946efb971807b0cbf407fc6f7a92cdbc297
Caleb James DeLisle <cjd@cjdns.fr>
2017-07-16 02:44:19 +0200

    support systems which compile with and without leading _

--- safecall.py
+++ safecall.py
@@ -7,14 +7,21 @@ def csum(s):
     return zlib.crc32(s) & 0xffff
 
 def main():
-    bracketCount = 0
-    lastFunc = 'NONE'
+    leadingUnderscore = False
+    lines = []
     for line in fileinput.input():
+        lines.append(line)
+        m = line.split()
+        if len(m) > 0 and (m[0] == 'call' or m[0] == 'callq'):
+            if m[1] in [ '_printf', '___stack_chk_fail', '_execve', 'puts', '_read']:
+                leadingUnderscore = True
+    for line in lines:
         m = line.split()
         print line[:-1]
         if len(m) > 0 and (m[0] == 'call' or m[0] == 'callq'):
-            name = m[1][1:]
-            code = csum(m[1][1:])
+            name = m[1]
+            if leadingUnderscore: name = name[1:]
+            code = csum(name)
             print '\t.byte 0xeb, 0x02, ' + hex(code & 255) + ', ' + hex(code>>8) + ' # safecall(' + name + ')'
 
 main()
